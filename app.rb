d7e90b9a8368c847ab53fbc547280113070d85f2
Chitroopa Manikkavasagam and Jesse Hoare Evans <chitroopa@>
2017-05-03 13:34:43 -0700

    refactor index page

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ also_reload('./**/*.rb')
 DB = PG.connect({:dbname=>'to_do_test'})
 
 get('/') do
+  @lists = List.all()
   erb(:index)
 end
 
@@ -18,9 +19,9 @@ get('/lists') do
   erb(:lists)
 end
 
-get('/lists/new') do
-  erb(:list_form)
-end
+# get('/lists/new') do
+#   erb(:list_form)
+# end
 
 get('/lists/:id') do
   @list = List.find(params.fetch("id").to_i())
@@ -36,13 +37,19 @@ post("/lists/:id") do
   erb(:list)
 end
 
+post("/tasks") do
+  description = params.fetch("description")
+  list_id = params.fetch("list_id").to_i()
+  @list = List.find(list_id)
+  @task = Task.new({:description => description, :list_id => list_id})
+  @task.save()
+  erb(:list)
+end
+
 post('/lists') do
   name = params.fetch('name')
-  task = params.fetch('task')
   list = List.new({:name => name , :id=> nil})
   list.save()
-  task = Task.new({:description => task, :list_id => list.id()})
-  task.save()
   @lists = List.all()
   erb(:lists)
 end
--- spec/todo_integration_spec.rb
+++ spec/todo_integration_spec.rb
@@ -6,9 +6,8 @@ set(:show_exceptions, false)
 describe('adding a new list', {:type => :feature}) do
   it('allows a user to click a list to see the tasks and details for it') do
     visit('/')
-    click_link('Add New List')
     fill_in('name', :with =>'Epicodus Work')
-    click_button('Click')
+    click_button('Add list')
     expect(page).to have_content('Epicodus Work')
   end
 end
